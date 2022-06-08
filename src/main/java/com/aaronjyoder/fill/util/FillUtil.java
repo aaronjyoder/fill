6b330512b31e235d3c37688125a2ef90417d7f8e
Aaron J Yoder <aaronjyoder@gmail.com>
2022-06-08 05:35:15 -0400

    Create FillUtil.java

---
+++ src/main/java/com/aaronjyoder/fill/util/FillUtil.java
@@ -0,0 +1,12 @@
+package com.aaronjyoder.fill.util;
+
+public class FillUtil {
+
+  private FillUtil() {
+  }
+
+  public static boolean isBounded(int x, int y, int width, int height) {
+    return x >= 0 && y >= 0 && x < width && y < height;
+  }
+
+}
